e7c6456599652d83593055caeb41f62462ec680c
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-17 16:14:09 +0000

    country select list created and displaying

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="app">
-
+    <h1>Countries Of The World</h1>
+    <div class="">
+      <country-form :countries="countries"></country-form>
+    </div>
   </div>
 </template>
 
 <script>
+import CountryForm from './components/countryForm.vue'
 
 export default {
   name: 'app',
   components: {
-
+    "country-form": CountryForm
   },
   data() {
     return {
-      countriesArray: []
+      countries: []
     }
   },
   mounted(){
     fetch('https://restcountries.eu/rest/v2/all')
       .then(response => response.json())
-      .then(objectsArray => this.countriesArray = objectsArray)
+      .then(objectsArray => this.countries = objectsArray)
   }
 }
 </script>
---
+++ src/components/countryForm.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <select class="" name="">
+    <option v-for="country in countries">{{country.name}}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'country-form',
+  props: ['countries']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
