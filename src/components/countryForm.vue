530a6bda54c51b747f41c59d10ae9024ec9b135f
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-02-09 11:38:06 +0000

    add placeholder to select

--- src/components/countryForm.vue
+++ src/components/countryForm.vue
@@ -1,5 +1,6 @@
 <template lang="html">
   <select v-on:change="handleChange" v-model="selectedCountryCode">
+    <option disabled selected>Select a country...</option>
     <option v-for="country in countries" :value="country.alpha3Code">{{country.name}}</option>
   </select>
 </template>
